b48ce0489388adbdb3cd82633dd266b213726e2c
Carine Morel <carine.morel.pro@gmail.com>
2018-07-03 15:19:26 +0200

    Add comment in test.ml and remove an exercise in index.json

--- exercises/sampler-built-in-types/test.ml
+++ exercises/sampler-built-in-types/test.ml
@@ -1,18 +1,17 @@
 open Test_lib
 open Report
 
-(*A first way to grade this function with randomly generated tests 
-  is by using the default sampler.*)
+(* A basic way to grade a function with automatically generated tests: 
+   using the default sampler. *)
 let exercise_1 =
   Section ([ Text "Function: "; Code "identity" ],
            test_function_1_against_solution
              [%ty: int -> int] "identity"
-             ~sampler:(fun () -> Random.int 32)
              ~gen:5 [0]
           )
 
-
-(*Using the optional argument ~sampler*)
+(* With a user-defined sampler : 
+   Method 1: Using the optional argument ~sampler *)
 let exercise_2 =
   Section ([ Text "Function: "; Code "pi1" ],
            test_function_2_against_solution
@@ -23,7 +22,9 @@ let exercise_2 =
           )
 
 
-(*Redefine sampler*)
+(* With a user-defined sampler :
+   Method 2: redefine sampler with respect to naming rule
+   val sample_<type>: unit -> <type>  *)
 let sample_int () = Random.int 10 + 42
 let exercise_3 =
   Section ([ Text "Function: "; Code "pi1" ],
@@ -33,7 +34,7 @@ let exercise_3 =
              []
           )
 
-(*Example for a parametric type composed of built-in types*)
+(* An example for a parametric type composed of built-in types *)
 let exercice_4 =
   Section ([ Text "Function: "; Code "push" ],
            test_function_2_against_solution
