a9b4bde11b7a3a5ca56f1ecb7407a7fc7156f0af
Carine Morel <carine.morel.pro@gmail.com>
2018-08-01 19:15:12 +0200

    typos

--- exercises/step-5-examples/test.ml
+++ exercises/step-5-examples/test.ml
@@ -6,18 +6,17 @@ let exercise_1 =
   Section ([ Text "Function: "; Code "identity" ], 
            test_function_1_against_solution 
              [%ty: int -> int] (* Type of the tested function *)
-             "identity"        (* identifier of the tested function *)
+             "identity"        (* Identifier of the tested function *)
              ~gen:10           (* number of automatically generated tests *)
              ~sampler:(fun () -> Random.int 42) (* sampler *)
              [0 ; 42]          (* List of tested inputs *)
     )
 
-(* Trivial example with automatic test generations *)
 let exercise_2 = 
   Section ([ Text "Function: "; Code "identity" ], 
            test_function_1 
              [%ty: int -> int] (* Type of the tested function *)
-             "identity"        (* identifier of the tested function *)
+             "identity"        (* Identifier of the tested function *)
              [0, 0, "", "prout" ;
               42, 42, "prout", ""]  (* List of tests *)
     )
@@ -26,24 +25,24 @@ let exercise_2 =
 let exercise_3 = 
   Section ([ Text "Function: "; Code "hello" ], 
            test_function_1_against_solution 
-             [%ty: unit -> unit] (* Type of the tested function *)
+             [%ty: unit -> unit] 
              ~test:test_ignore   (* Function that compares outputs *)
              ~test_stdout:io_test_equals  (* Function that compares standart outputs *)
-             "hello"             (* identifier of the tested function *)
+             "hello"             
              ~gen:0         
              [()]           
           )
 
-let exercise_4 = 
-  Section ([ Text "Function: "; Code "hello" ], 
-           test_function_1 
-             [%ty: unit -> unit] (* Type of the tested function *)
+let exercise_4 =
+  Section ([ Text "Function: "; Code "hello" ],
+           test_function_1
+             [%ty: unit -> unit] 
              ~test:test_ignore   (* Function that compares outputs *)
              ~test_stdout:io_test_equals  (* Function that compares standart outputs *)
-             "hello"             (* identifier of the tested function *)
-             [(), (), "Hello world!", ""]           
+             "hello"             
+             [(), (), "Hello world!", ""]
           )
-    
+
   
 let () = 
   set_result @@
