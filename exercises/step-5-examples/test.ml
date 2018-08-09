e0217bcc2ca93127c5e3e0c2d188d92733fbc8c7
Carine Morel <carine.morel.pro@gmail.com>
2018-08-09 04:10:14 +0200

    Update wwith grade_function_...

--- exercises/step-5-examples/test.ml
+++ exercises/step-5-examples/test.ml
@@ -3,46 +3,38 @@ open Report
 
 (* Trivial example with automatic test generations *)
 let exercise_1 = 
-  Section ([ Text "Function: "; Code "identity" ], 
-           test_function_1_against_solution 
-             [%ty: int -> int] (* Type of the tested function *)
-             "identity"        (* Identifier of the tested function *)
-             ~gen:10           (* number of automatically generated tests *)
-             ~sampler:(fun () -> Random.int 42) (* sampler *)
-             [0 ; 42]          (* List of tested inputs *)
-    )
+  grade_function_1_against_solution 
+    [%ty: int -> int] (* Type of the tested function *)
+    "identity"        (* Identifier of the tested function *)
+    ~gen:10           (* number of automatically generated tests *)
+    ~sampler:(fun () -> Random.int 42) (* sampler *)
+    [0 ; 42]          (* List of tested inputs *)
 
 let exercise_2 = 
-  Section ([ Text "Function: "; Code "identity" ], 
-           test_function_1 
-             [%ty: int -> int] (* Type of the tested function *)
-             "identity"        (* Identifier of the tested function *)
-             [0, 0, "", "prout" ;
-              42, 42, "prout", ""]  (* List of tests *)
-    )
+  grade_function_1 
+    [%ty: int -> int] (* Type of the tested function *)
+    "identity"        (* Identifier of the tested function *)
+    [0, 0, "", "" ;
+     42, 42, "", ""]  (* List of tests *)
+ 
   
 (* Simple example with standart outputs *)
 let exercise_3 = 
-  Section ([ Text "Function: "; Code "hello" ], 
-           test_function_1_against_solution 
-             [%ty: unit -> unit] 
-             ~test:test_ignore   (* Function that compares outputs *)
-             ~test_stdout:io_test_equals  (* Function that compares standart outputs *)
-             "hello"             
-             ~gen:0         
-             [()]           
-          )
-
+  grade_function_1_against_solution 
+    [%ty: unit -> unit] 
+    ~test:test_ignore   (* Function that compares outputs *)
+    ~test_stdout:io_test_equals  (* Function that compares standart outputs *)
+    "hello"             
+    ~gen:0         
+    [()]           
+    
 let exercise_4 =
-  Section ([ Text "Function: "; Code "hello" ],
-           test_function_1
-             [%ty: unit -> unit] 
-             ~test:test_ignore   (* Function that compares outputs *)
-             ~test_stdout:io_test_equals  (* Function that compares standart outputs *)
-             "hello"             
-             [(), (), "Hello world!", ""]
-          )
-
+  grade_function_1
+    [%ty: unit -> unit] 
+    ~test:test_ignore   (* Function that compares outputs *)
+    ~test_stdout:io_test_equals  (* Function that compares standart outputs *)
+    "hello"             
+    [(), (), "Hello world!", ""]
   
 let () = 
   set_result @@
